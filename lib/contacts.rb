515d4fa5cc372442de67cb14401fd06cc26a4086
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-08 21:12:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,8 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    
-    if contacts == "Freddy Mercury"
+    binding.pry
+    #if contacts == "Freddy Mercury"
   data.each do |item, value|
   
   # if item == :favorite_ice_cream_flavors
